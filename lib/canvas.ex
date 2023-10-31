a0def305ab50bc431daae17ab873a566224d3284
Bruce Tate <bruce@grox.io>
2023-10-31 07:09:07 -0400

    add tetro render

--- lib/canvas.ex
+++ lib/canvas.ex
@@ -4,23 +4,33 @@ defmodule Canvas do
     <svg
       width="100" height="200"
       xmlns="http://www.w3.org/2000/svg">
-      #{draw(contents)}
+      #{draw(contents, 10)}
     </svg>
     """
     |> Kino.Image.new(:svg)
   end
 
-  def draw({row, col}) do
-    x = (col - 1) * 10
-    y = (row - 1) * 10
-    width = 10
+  def tetromino(contents) do
+    """
+    <svg
+      width="100" height="100"
+      xmlns="http://www.w3.org/2000/svg">
+      #{draw(contents, 25)}
+    </svg>
+    """
+    |> Kino.Image.new(:svg)
+  end
+
+  def draw({row, col}, width) do
+    x = (col - 1) * width
+    y = (row - 1) * width
 
     ~s[<rect x="#{x}" y="#{y}" width="#{width}" height="#{width}" fill="black" />]
   end
 
-  def draw(points) when is_list(points) do
+  def draw(points, width) when is_list(points) do
     points
-    |> Enum.map(&draw/1)
+    |> Enum.map(&draw(&1, width))
     |> Enum.join("\n")
   end
 end
